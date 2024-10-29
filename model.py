6d285e10df26a7aca6bed6ddd41253809ed3f143
Bandari Suneeth Kumar Sharma <suneethbandari2609@gmail.com>
2024-10-29 17:25:18 +0530

    files other than models

---
+++ model.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:227d81723a26e28910227f2b55708c5a3f165b285f83447bb96f6b34de455c23
+size 3334
---
+++ singleface.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:f5b66dd06ed151407bf2c9e061769c5917b49de658bae5320ac4101db7f8795c
+size 3856
